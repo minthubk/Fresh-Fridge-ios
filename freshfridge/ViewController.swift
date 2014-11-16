240826f7b2fd2587a5c4b882a62e95727da42ebc
Mark Jackson <jacks205@mail.chapman.edu>
2014-11-16 03:08:25 -0800

    Initial commit with blank project

---
+++ freshfridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  freshfridge
+//
+//  Created by Mark Jackson on 16/11/2014.
+//  Copyright (c) 2014 Mark Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
